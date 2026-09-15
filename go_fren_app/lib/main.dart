f8ad0baa9610afcfce637fa7dfd76934bf4f1357
pohanxz <ivan.saputra147@gmail.com>
2026-09-15 08:08:42 +0000

    Initialize Supabase in Go Fren

--- go_fren_app/lib/main.dart
+++ go_fren_app/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'supabase_config.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    publishableKey: SupabaseConfig.publishableKey,
+  );
+
   runApp(const GoFrenApp());
 }
 
